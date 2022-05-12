7ea39f3e987fd28053c601f4244997f798b8995b
KhaledAmmer <k.tayseer.amer@gmail.com>
2022-05-12 21:45:45 +0300

    finish getAllPosts function

--- frontend/src/components/Homepage/index.jsx
+++ frontend/src/components/Homepage/index.jsx
@@ -1,35 +1,64 @@
 import { UserController } from "../../controllers/user";
 import React, { useState } from "react";
-import { useEffect} from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { PostController } from "../../controllers/posts";
 
 export const Homepage = () => {
-  const [postContent,setPostContent] = useState("");
-  const [posts,setPosts] = useState([]);
-  useEffect(() => {
-  
-  }, []);
-  const navigate =useNavigate()
+  const [postContent, setPostContent] = useState("");
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {}, []);
+  const navigate = useNavigate();
   return (
     <div>
-      <button onClick={()=>{
-        navigate("/s")
-      }}>Go to Friend Page</button>
-      <br/>
-      <br/>
-      <textarea onChange={(e)=>{
-        setPostContent(e.target.value)
-      }}></textarea>
-      <br/>
-      <button onClick={async()=>{
-        console.log("ASdasdasdasd");
-       
-        setPosts( [...posts,await PostController.createNewPost({content:postContent})])
-      }}>Create New Post</button>
-      {posts.length !== 0 ? posts.map(post=>{
-        return <p> {post.content}</p>
-      }):<></>}
+      <button
+        onClick={() => {
+          navigate("/s");
+        }}
+      >
+        Go to Friend Page
+      </button>
+      <br />
+      <br />
+      <textarea
+        onChange={(e) => {
+          setPostContent(e.target.value);
+        }}
+      ></textarea>
+      <br />
+      <button
+        onClick={async () => {
+          console.log("ASdasdasdasd");
+
+          setPosts([
+            ...posts,
+            await PostController.createNewPost({ content: postContent }),
+          ]);
+        }}
+      >
+        Create New Post
+      </button>
+      <button
+        onClick={async () => {
+          console.log("ASdasdasdasd");
+
+          setPosts([
+            ...posts,
+            ...await PostController.getAllPosts(),
+          ]);
+        }}
+      >
+        get All Posts
+      </button>
+
+      {posts.length !== 0 ? (
+        posts.map((post) => {
+          
+          return <p key={post._id}> {post.content}</p>;
+        })
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
--- frontend/src/controllers/posts.js
+++ frontend/src/controllers/posts.js
@@ -12,8 +12,19 @@ export class PostController {
           headers: { authorization: `Bearer ${Info.token}` },
         }
       );
-    
-       return response.data.post;
+      return response.data.post;
+    } catch (error) {
+      return error.response.data.message;
+    }
+  }
+  static async getAllPosts() {
+    const userId = Info.userId;
+
+    try {
+      const response = await axios.get(`${Info.hostUrl}/posts/${userId}`, {
+        headers: { authorization: `Bearer ${Info.token}` },
+      });
+      return response.data.posts;
     } catch (error) {
       return error.response.data.message;
     }
